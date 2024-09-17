d4e33e1d3b58dddcb0f07958fee360666669821c
RobyLedda94 <robyledda01@yahoo.it>
2024-09-17 14:30:10 +0200

    [UPD] dichiarata classe e variabili d'istanza

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+// definisco la classe movie
+
+class Movie {
+    // dichiaro le variabili di istanza
+    public $titolo;
+    public $anno; 
+    public $genere;
+    public $cast;
+}
+?>
