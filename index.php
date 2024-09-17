e2696238654dea74f7caaf6c7d265e88cea4b122
RobyLedda94 <robyledda01@yahoo.it>
2024-09-17 14:54:03 +0200

    [UPD] create le istanze della classe movie, corretta la sintassi nel blocco di codice del costruttore

--- index.php
+++ index.php
@@ -9,11 +9,16 @@ class Movie {
     public $cast;
 
     // definisco il costruttore
-    public function __construct($_titolo, $_anno, $genere, $_cast) {
-        $this->titolo = $titolo;
-        $this->anno = $anno;
-        $this->genere = $genere;
-        $this->cast = $cast;
+    public function __construct($_titolo, $_anno, $_genere, $_cast) {
+        $this->titolo = $_titolo;
+        $this->anno = $_anno;
+        $this->genere = $_genere;
+        $this->cast = $_cast;
     }
 }
+
+// creo le istanze della classe Movie
+    $firs_movie = new Movie ("Will Hunting - Genio Ribelle", 1997, "Drammatico", "Matt Damon, Ben Affleck, Robin Williams");
+    $second_movie = new Movie ("Il Padrido", 1972, "Giallo", "Al Pacino, Marlon Brando");
+
 ?>
