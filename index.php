59cf1b1344e6f66bdd7e31ef0175cef51d7c81e1
RobyLedda94 <robyledda01@yahoo.it>
2024-09-17 14:36:55 +0200

    [UPD] definito il costruttore

--- index.php
+++ index.php
@@ -7,5 +7,13 @@ class Movie {
     public $anno; 
     public $genere;
     public $cast;
+
+    // definisco il costruttore
+    public function __construct($_titolo, $_anno, $genere, $_cast) {
+        $this->titolo = $titolo;
+        $this->anno = $anno;
+        $this->genere = $genere;
+        $this->cast = $cast;
+    }
 }
 ?>
